dd17c22215372cdd8b39152232a50057d99ec197
sunkang-kim <tjsrkd91@gmail.com>
2023-04-22 00:11:57 +0900

    test

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# ORM을 적용하기 위한 데이터베이스 설정 
 import os
 
 BASE_DIR = os.path.dirname(__file__)
@@ -6,5 +7,5 @@ BASE_DIR = os.path.dirname(__file__)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR,'pybo.db'))
 # 설정에 의해 SQLite 데이터베이스가 사용되고, 데이터베이스 파일은 프로젝트 홈 디렉터리 바로 밑에 pybo.db로 저장됨.
 
-# SQLAlchemy의 이벤트를 처리하는 옵션
+# SQLAlchemy의 이벤트를 처리하는 옵션 - 해당 옵션은 현 프로젝트에 사용되지 않아 False로 설정.
 SQLALCHEMY_TRACK_MODIFICATIONS = False
