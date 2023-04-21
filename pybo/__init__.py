66d2cd159e3b7143af64606379d7474ddfc57792
sunkang-kim <tjsrkd91@gmail.com>
2023-04-22 00:09:43 +0900

    답변 달기

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -13,14 +13,16 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
 
-    # ORM
+    # ORM(object relational mapping) - 파이썬 문법만으로 데이터베이스를 다룰수있음.
     db.init_app(app)
     migrate.init_app(app, db)
     from . import models
 
     # bp 객체 등록 - 블루프린트
-    from .views import main_views, question_views   # .view 폴더[패키지] 안의 main_views 팡리
+    # .views 폴더 안에 각 페이지 view를 담당하는 .py를 블루프린트 객체로 등록
+    from .views import main_views, question_views, answer_views   
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
 
     return app
