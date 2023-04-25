be8b9ee8031a6d2eb7f793dd55800263895db8db
sunkang-kim <tjsrkd91@gmail.com>
2023-04-25 22:29:29 +0900

    로그인 로그아웃 구현

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, url_for, render_template, flash, request
-from werkzeug.security import generate_password_hash
+from flask import Blueprint, url_for, render_template, flash, request, session, g
+from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
 from pybo import db
-from pybo.forms import UserCreateForm
+from pybo.forms import UserCreateForm, UserLoginForm
 from pybo.models import User
+
+import functools
 
 # /auth/ 로 시작하는 URL 호출시 auth.views.py의 함수가 호출되도록하는 블루프린트 객체 생성
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-
+# 회원가입
 @bp.route('/signup/', methods=('GET', 'POST'))
 def signup():
     form = UserCreateForm()
@@ -25,3 +27,55 @@ def signup():
         else:
             flash('이미 존재하는 사용자입니다.')
     return render_template('auth/signup.html', form=form)
+
+# 로그인
+@bp.route('/login/', methods=('GET', 'POST'))
+def login():
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        error = None
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            error = "존재하지 않는 사용자입니다."
+        elif not check_password_hash(user.password, form.password.data):
+            error = "비밀번호가 올바르지 않습니다."
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            _next = request.args.get('next', '')
+            if _next:
+                return redirect(_next)
+            else:
+                return redirect(url_for('main.index'))
+        flash(error)
+    return render_template('auth/login.html', form=form)
+
+# 세션
+# @bp.before_app_request 애너테이션이 적용된 함수는 라우팅 함수보다 항상 먼저 실행
+@bp.before_app_request      
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
+        # g.user에는 User 객체가 저장된다.
+
+
+# 로그아웃 
+@bp.route('/logout/')
+def logout():
+    # 세션의 모든 값 삭제
+    session.clear()
+    return redirect(url_for('main.index'))
+
+
+# login_required 데코레이터 함수 생성
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.user is None:
+            _next = request.url if request.method == 'GET' else ''
+            return redirect(url_for('auth.login', next=_next))
+        return view(*args, **kwargs)
+    return wrapped_view
